8265831735c5a7d50311dc84cb032369c76880d5
Theo Barbara <theobarbara@terra.com.br>
2018-05-02 16:15:32 -0300

    Theo
    Classe Inimigo criada

--- Projeto_Final.py
+++ Projeto_Final.py
@@ -37,6 +37,22 @@ class Torre(pygame.sprite.Sprite):
         vida -= dano
         
         return vida
+    
+class Inimigo(pygame.sprite.Sprite):
+    
+    def __init__(self, imagem, posx, posy):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load(imagem)
+        self.rect=self.image.get_rect()
+        self.rect.x=posx
+        self.rect.y=posy
+    
+    def ativa_inimigo(self, imagem):
+        self.image = pygame.image.load(imagem)
+        
+    def move_inimigo(self, velocidade):
+        self.rect.x += velocidade
 
 # início
     

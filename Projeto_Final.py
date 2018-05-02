43198dfa720a2e955db3366cd3c6a2d81af119a9
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-05-02 15:53:42 -0300

    Maurício
    Alterei o dano para -=; coloquei o personagem em movimento e coloquei um relógio para que o personagem ande mais devagar.

--- Projeto_Final.py
+++ Projeto_Final.py
@@ -17,7 +17,7 @@ class Boneco(pygame.sprite.Sprite):
         self.image=pygame.image.load(imagem)
     
     def move(self, velocidade):
-        self.rect.x+=velocidade
+        self.rect.x += velocidade
 
 class Torre(pygame.sprite.Sprite):
     
@@ -34,7 +34,7 @@ class Torre(pygame.sprite.Sprite):
     
     def Vida_torre(self,dano):
         vida = 10000
-        vida += -dano
+        vida -= dano
         
         return vida
 
@@ -47,8 +47,8 @@ pygame.display.set_caption('Tower defense')
 
 fundo= pygame.image.load("cenario.jpeg").convert()
 
-personagem1 = Boneco('Naruto.png', (30),(300))
-personagem1_group = pygame.sprite.Group()
+personagem1 = Boneco('Naruto.png', 30,300)
+personagem_group = pygame.sprite.Group()
 
 
 
@@ -56,18 +56,22 @@ personagem1_group = pygame.sprite.Group()
 
 rodando = True
 while rodando:
+    relogio=pygame.time.Clock()
+    tempo=relogio.tick(50)
+    
     pressed_keys=pygame.key.get_pressed()
     if pressed_keys[K_w]:
-        naruto = personagem1
         personagem1.ativa_boneco('Naruto.png')
-        personagem1_group.add(personagem1)
+        personagem_group.add(personagem1)
+    personagem1.move(1)
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             rodando = False
             
     
     tela.blit(fundo, (0,0))
-    personagem1_group.draw(tela)
+    personagem_group.draw(tela)
     
     pygame.display.update()   
 

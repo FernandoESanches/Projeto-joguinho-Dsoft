bddceb3e392b8a35e5cf2f9b6e7bff858a2ff03f
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-05-04 10:01:18 -0300

    Maurício
    Coloquei uma torre

--- Projeto_Final.py
+++ Projeto_Final.py
@@ -63,38 +63,43 @@ pygame.display.set_caption('Tower defense')
 
 fundo= pygame.image.load("cenario.jpeg").convert()
 
-personagem1 = Boneco('goku.png', 10,250)
+personagem1 = Boneco('goku.png', 10,150)
 personagem_group = pygame.sprite.Group()
 inimigo1=Inimigo('Naruto.png',1100,351)
 inimigo_group=pygame.sprite.Group()
-
-
+torre=Torre("鳥居.png", 0,100)
+torre_group=pygame.sprite.Group()
+torre_group.add(torre)
 
 rodando = True
 while rodando:
     relogio=pygame.time.Clock()
     tempo=relogio.tick(50)
-    
-    pressed_keys=pygame.key.get_pressed()
-    if pressed_keys[K_w]:
-        personagem1.ativa_boneco('goku.png')
-        personagem_group.add(personagem1)
-    elif pressed_keys[K_q]:
-        inimigo1.ativa_inimigo('Naruto.png')
-        inimigo_group.add(inimigo1)
-    if pygame.sprite.spritecollide(personagem1,inimigo_group, False):
-        personagem1.move(0)
-        inimigo1.move_inimigo(0)
-    else:
-        personagem1.move(1)
-        inimigo1.move_inimigo(-3)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             rodando = False
+        if (event.type==pygame.KEYDOWN):
+            pygame.key.get_repeat()
+            if (event.key==pygame.K_w):
+                 personagem1.ativa_boneco('goku.png')
+                 personagem_group.add(personagem1)
+            elif (event.key==pygame.K_q):
+                inimigo1.ativa_inimigo('Naruto.png')
+                inimigo_group.add(inimigo1)
+    if pygame.sprite.spritecollide(personagem1,inimigo_group, False):
+        personagem1.move(0)
+    else:
+        personagem1.move(1)
+    if pygame.sprite.spritecollide(personagem1,inimigo_group, False):
+        inimigo1.move_inimigo(0)
+    else:
+        inimigo1.move_inimigo(-3)        
         
     tela.blit(fundo, (0,0))
     personagem_group.draw(tela)
     inimigo_group.draw(tela)
+    torre_group.draw(tela)
     
     pygame.display.update()   
 
